be5ca5e3e57579c5192c4ebda9e6a63e3a4884d3
Rodwell  Matchon <rmatchon@addunet.ph>
2024-01-29 19:21:44 +0800

    added node modules

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+const express = require('express')
+const app = express()
+
+const port = 8000
+
+app.listen(port, () => {
+    console.log(`Server connected to port ${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.send(`
+        <div>
+            Hello World!
+        </div>
+    `)
+
+    console.log("request received")
+})
